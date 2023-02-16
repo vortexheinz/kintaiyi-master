119fd6f0a85db66da3d1a8a746f8cd62a5739101
vortexheinz <vortexheinz@users.noreply.github.com>
2023-02-16 16:46:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import os, urllib
 
 def get_file_content_as_string(path):
     #url = 'https://raw.githubusercontent.com/kentang2017/kintaiyi/master/' + path
-    url = 'https://github.com/vortexheinz/kintaiyi-master/blob/master/' + path
+    url = 'https://github.com/vortexheinz/kintaiyi-master/blob/master' + path
     response = urllib.request.urlopen(url)
     return response.read().decode("utf-8")
 
