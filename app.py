5079448d6b36cf247b5a156ff63ca89dd5cff059
vortexheinz <vortexheinz@users.noreply.github.com>
2023-02-17 03:07:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -300,7 +300,7 @@ with disaster:
    
 with ins:
     st.header('使用說明2')   
-    print("路徑{}路徑\n",get_file_content_as_string("instruction2.md"))
+    #print("路徑{}路徑\n",get_file_content_as_string("instruction2.md"))
     st.markdown(get_file_content_as_string("instruction2.md"))
     #outputx = st.empty()
     #outputx = get_file_content_as_string("instruction2.md")
